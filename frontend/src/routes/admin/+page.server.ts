1d6ec0408b31545259517af16b0189d73e9b39e3
Loïc Vandenberghe <loiclovitana@gmail.com>
2025-04-29 07:31:54 +0200

    implement error handling and log for requests

--- frontend/src/routes/admin/+page.server.ts
+++ frontend/src/routes/admin/+page.server.ts
@@ -1,38 +1,67 @@
+import { fail } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import type {Actions } from './$types';
+import type { Actions } from './$types';
 
-import { adminLogin } from '$lib/hmTrackerApiClient'
+import { adminLogin, getOperation, startStatsLoading } from '$lib/hmTrackerApiClient'
 
 export const load: PageServerLoad = async ({ cookies }) => {
-    const token = cookies.get('admin_token');
-    const admin_user = cookies.get('admin_user');
-    return { admin_token: token, admin_user: admin_user };
+    const admin_token: string | undefined = cookies.get('admin_token');
+    const admin_user: string | undefined = cookies.get('admin_user');
+    const operation: Promise<string> = admin_token === undefined ? new Promise(() => '') : getOperation(admin_token);
+    return { admin_token: admin_token, admin_user: admin_user, operation: operation };
 };
 
 export const actions = {
     login: async ({ cookies, request }) => {
+        logRequest(request);
+        if (cookies.get('admin_token')) {
+            return fail(403, { error: 403, errorMessage: "User is already logged" });
+        }
+
         const data: FormData = await request.formData();
         const admin_user: string | undefined = data.get('user')?.toString();
         const password: string | undefined = data.get('password')?.toString();
 
         if (admin_user === undefined || password === undefined) {
-            return { logged: false };
+            fail(401, { error: 401, errorMessage: "The user or the password was not provided for login" });
         }
 
-        const tokenResponse = await adminLogin(admin_user, password);
+        const tokenResponse = await adminLogin(admin_user as string, password as string);
         if (!tokenResponse.ok) {
-            return { success: false };
+            return fail(tokenResponse.status, { error: tokenResponse.status, errorMessage: await tokenResponse.text() });
         }
 
         let tokenData = await tokenResponse.json()
         let token = tokenData.access_token;
         cookies.set('admin_token', token, { path: '/admin' });
-        cookies.set('admin_user', admin_user, { path: '/admin' });
-        return { success: true };
+        cookies.set('admin_user', admin_user as string, { path: '/admin' });
+        return { logged: true };
     },
-    logout:async({ cookies, request }) =>{
-        cookies.delete('admin_token',{ path: '/admin' });
-        cookies.delete('admin_user',{ path: '/admin' });
+    logout: async ({ cookies, request }) => {
+        logRequest(request);
+        cookies.delete('admin_token', { path: '/admin' });
+        cookies.delete('admin_user', { path: '/admin' });
+    },
+    start_stats_loading: async ({ cookies, request }) => {
+        logRequest(request);
+        const admin_token: string | undefined = cookies.get('admin_token');
+        if (admin_token === undefined) {
+            fail(401, {
+                error: 401,
+                errorMessage: 'Admin need to be logged in to execute operation',
+            });
+        }
+        const response =  await startStatsLoading(admin_token as string);
+        if (!response.ok) {
+            return fail(response.status, { error: response.status, errorMessage: await response.text() });
+        }
+        return {started:true};
     }
 } satisfies Actions;
+
+function logRequest(request:Request){
+    const now = new Date();
+    let message : string =`${now.toLocaleTimeString("fr-CH")} [${request.method}] - ${request.url}`;
+    console.info(message);
+}
 
--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,16 +1,75 @@
-<script>
+<script lang="ts">
+	import { enhance } from '$app/forms';
 	import AdminLoginForm from '$lib/AdminLoginForm.svelte';
-	let { data } = $props();
+	import type { PageData, ActionData } from './$types';
+
+	let { data, form }: { data: PageData; form: ActionData } = $props();
+
+	let start_loading_form: HTMLFormElement;
+
+	function confirmStartLoad() {
+		const confirmed = window.confirm(
+			'Are you sure you want to start manual loading?\n!! CAN TAKE A FEW MINUTES !!  '
+		);
+		if (confirmed) {
+			start_loading_form.submit();
+		}
+	}
 </script>
 
+{#if form?.error}
+	<div class="info error-block">
+		<div class="info-title"><b>Error:</b> {form?.error}</div>
+		<div class="info-message">{form?.errorMessage}</div>
+	</div>
+{/if}
+{#if form?.logged}
+	<div class="info success-block">
+		<div class="info-message">Logged successfully</div>
+	</div>
+{/if}
 <main class="container">
 	<h1 style="text-align: center;">Server Administration</h1>
 	<hr />
 	{#if data.admin_token}
-		<h2>Logged in as "<i>{data.admin_user}</i>"</h2>
-		<form method="POST" action="?/logout">
-			<button type="submit"> Log out </button>
-		</form>
+		<div class="logout">
+			<h3>Logged in as "<i>{data.admin_user}</i>"</h3>
+			<form method="POST" action="?/logout">
+				<button type="submit"> Log out </button>
+			</form>
+		</div>
+		<hr />
+
+		<div class="status">
+			<h3>Current operation</h3>
+			{#await data.operation}
+				<article aria-busy="true"></article>
+			{:then operation}
+				{#if operation}
+					<article>{operation}</article>
+				{:else}
+					<article>No operation in progress</article>
+				{/if}
+			{/await}
+
+			<p><i></i></p>
+		</div>
+		<hr />
+		<div class="execute">
+			<h3>Load players stats</h3>
+			<p>
+				Load the latest statistics of hockey players from hockey manager website into the database
+			</p>
+			
+			<form method="POST" action="?/start_stats_loading" bind:this={start_loading_form}>
+				<button onclick={confirmStartLoad}>Start load</button>
+				{#if form?.started}
+				<div class="info success-block">
+					<div class="info-message">Started loading the players</div>
+				</div>
+			{/if}
+			</form>
+		</div>
 	{:else}
 		<AdminLoginForm />
 	{/if}
