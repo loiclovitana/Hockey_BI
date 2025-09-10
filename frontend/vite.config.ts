1a4c70742e297a503cc8a878716975c1a5d71665
loiclovitana <loiclovitana@gmail.com>
2025-09-10 11:16:26 +0200

    misc: separate chunk for build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ['@mui/material', '@mui/icons-material'],
+          'mui-x': ['@mui/x-charts', '@mui/x-data-grid'],
+          emotion: ['@emotion/react', '@emotion/styled']
+        }
+      }
+
+    }
+  }
 });
